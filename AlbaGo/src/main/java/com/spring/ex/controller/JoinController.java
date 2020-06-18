8ae6574bacf53c5d52e1b40d520bf7a69842c90b
yeseul <yeseul@Yeseul>
2020-06-18 19:16:54 +0900

    0618-개인 아이디찾기 성공했어요! find_id find_individual_result JoinController JoinDAO

--- AlbaGo/src/main/java/com/spring/ex/controller/JoinController.java
+++ AlbaGo/src/main/java/com/spring/ex/controller/JoinController.java
@@ -1,23 +1,27 @@
 package com.spring.ex.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.ex.dao.JoinDAO;
 import com.spring.ex.dto.Enterprise;
-import com.spring.ex.dto.Individual;
 import com.spring.ex.validator.EnterpriseValidator;
 
 @Controller
 @RequestMapping("/join")
 public class JoinController {
-	
+
 	@Autowired
 	JoinDAO joinDAO;
-	
+
 	@RequestMapping("/login") // 로그인
 	public String login() {
 		return "/join/login";
@@ -26,6 +30,32 @@ public class JoinController {
 	@RequestMapping("/find_id") // 아이디 찾기
 	public String find_id() {
 		return "/join/find_id";
+	}
+
+	@RequestMapping("/find_id/find_id_individual_result") // 개인 아이디 찾기 성공/
+	public String find_id_individual_result(Model m, HttpServletRequest request) {
+		String page = "/join/find_id"; // 아이디 찾기 폼으로 ,
+
+		String name = request.getParameter("name");
+		String phone = request.getParameter("phone");
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("name", name);
+		map.put("phone", phone);
+
+		String selectId = joinDAO.selectIndividualId(map);
+		System.out.println(selectId);
+		
+		if (!selectId.equals("")) {
+			m.addAttribute("id", selectId);
+		
+			System.out.println("DB에서 select 성공");
+			page = "/join/find_id_individual_result";// db값넣기 성공시 아이디확인 페이지로
+		} else {
+			System.out.println("DB에 select 실패");
+		}
+
+		return page;
 	}
 
 	@RequestMapping("/find_pw") // 비밀번호 찾기
@@ -45,24 +75,23 @@ public class JoinController {
 
 	@RequestMapping("/join_enterprise/join_result") // 기업회원가입 완료
 	public String join_enterprise_result(@ModelAttribute("enterprise") Enterprise enterprise, BindingResult result) {
-		String page = "/join/join_enterprise"; //회원가입 폼으로 
+		String page = "/join/join_enterprise"; // 회원가입 폼으
 
 		EnterpriseValidator validator = new EnterpriseValidator();
 
 		validator.validate(enterprise, result);
 
 		if (result.hasErrors()) {
-			return page;//에러 있으면 그냥 회원가입 폼으로 
+			return page;// 에러 있으면 그냥 회원가입 폼으
 		}
-		
+
 		else {
 			int insertResult = joinDAO.insertEnterprise(enterprise);
-			
-			if(insertResult==1) {
+
+			if (insertResult == 1) {
 				System.out.println("DB에 값넣기 성공");
-				page = "/join/join_enterprise_result";//db값넣기 성공시 result페이지로
-			}
-			else {
+				page = "/join/join_enterprise_result";// db값넣기 성공시 result페이지로
+			} else {
 				System.out.println("DB에 값넣기 실패");
 			}
 		}
--- AlbaGo/src/main/java/com/spring/ex/dao/JoinDAO.java
+++ AlbaGo/src/main/java/com/spring/ex/dao/JoinDAO.java
@@ -1,9 +1,14 @@
 package com.spring.ex.dao;
 
+import java.util.HashMap;
+
 import com.spring.ex.dto.Enterprise;
 import com.spring.ex.dto.Individual;
 
 public interface JoinDAO {
 	int insertEnterprise(Enterprise enterprise);
 	int insertIndividual(Individual individual);
+	
+	
+	String selectIndividualId(HashMap<String , String > map);
 }
