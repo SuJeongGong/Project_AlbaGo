3990a66eb5882ba634c33d7a4b5ce806dbc66379
yeseul <yeseul@Yeseul>
2020-06-19 13:31:25 +0900

    0619-개인,기업 비밀번호 초기화 가능

--- AlbaGo/src/main/java/com/spring/ex/controller/JoinController.java
+++ AlbaGo/src/main/java/com/spring/ex/controller/JoinController.java
@@ -1,3 +1,4 @@
+  
 package com.spring.ex.controller;
 
 import java.util.HashMap;
@@ -7,13 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.ex.dao.JoinDAO;
-import com.spring.ex.dto.Enterprise;
-import com.spring.ex.validator.EnterpriseValidator;
 
 @Controller
 @RequestMapping("/join")
@@ -32,60 +29,61 @@ public class JoinController {
 		return "/join/find_id";
 	}
 
-//	@RequestMapping("/find_id/find_id_individual_result") // 개인 아이디 찾기 성공/
-//	public String find_id_individual_result(Model m, HttpServletRequest request) {
-//		String page = "/join/find_id"; // 아이디 찾기 폼으로 ,
-//
-//		String name = request.getParameter("name");
-//		String phone = request.getParameter("phone");
-//
-//		HashMap<String, String> map = new HashMap<String, String>();
-//		map.put("name", name);
-//		map.put("phone", phone);
-//
-//		String selectId = joinDAO.selectIndividualId(map);
-//		System.out.println(selectId);
-//		
-//		if (!selectId.equals("")) {
-//			m.addAttribute("id", selectId);
-//		
-//			System.out.println("DB에서 select 성공");
-//			page = "/join/find_id_individual_result";// db값넣기 성공시 아이디확인 페이지로
-//		} else {
-//			System.out.println("DB에 select 실패");
-//		}
-//
-//		return page;
-//	}
-//	
-//	
-//	@RequestMapping("/find_id/find_id_enterprise_result") // 기업 아이디 찾기 성공/
-//	public String find_id_enterprise_result(Model m, HttpServletRequest request) {
-//		String page = "/join/find_id"; // 아이디 찾기 폼으로 ,
-//
-//		String manager_name = request.getParameter("name");
-//		String manager_phone = request.getParameter("phone");
-//
-//		HashMap<String, String> map = new HashMap<String, String>();
-//		map.put("manager_name", manager_name);
-//		map.put("manager_phone", manager_phone);
-//		System.out.println(manager_name);
-//		System.out.println(manager_phone);
-//
-//		String selectId = joinDAO.selectEnterpriseId(map);
-//		System.out.println(selectId);
-//		
-//		if (!selectId.equals("")) {
-//			m.addAttribute("id", selectId);
-//		
-//			System.out.println("DB에서 select 성공");
-//			page = "/join/find_id_enterprise_result";// db값넣기 성공시 아이디확인 페이지로
-//		} else {
-//			System.out.println("DB에 select 실패");
-//		}
-//
-//		return page;
-//	}
+	@RequestMapping("/find_id/find_id_individual_result") // 개인 아이디 찾기 성공/
+	public String find_id_individual_result(Model m, HttpServletRequest request) {
+		String page = "/join/find_id"; // 아이디 찾기 폼으로 ,
+
+		String name = request.getParameter("name");
+		String phone = request.getParameter("phone");
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("name", name);
+		map.put("phone", phone);
+
+		String selectId = joinDAO.selectIndividualId(map);
+		System.out.println(selectId);
+		
+		if (!selectId.equals("")) {
+			m.addAttribute("id", selectId);
+		
+			System.out.println("DB에서 select 성공");
+			page = "/join/find_id_individual_result";// db값넣기 성공시 아이디확인 페이지로
+		} else {
+			System.out.println("DB에 select 실패");
+		}
+
+		return page;
+	}
+	
+	
+	@RequestMapping("/find_id/find_id_enterprise_result") // 기업 아이디 찾기 성공/
+	public String find_id_enterprise_result(Model m, HttpServletRequest request) {
+		String page = "/join/find_id"; // 아이디 찾기 폼으로 ,
+
+		String manager_name = request.getParameter("name");
+		String manager_phone = request.getParameter("phone");
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("manager_name", manager_name);
+		map.put("manager_phone", manager_phone);
+		System.out.println(manager_name);
+		System.out.println(manager_phone);
+
+		String selectId = joinDAO.selectEnterpriseId(map);
+		System.out.println(selectId);
+		
+		if (!selectId.equals("")) {
+			m.addAttribute("id", selectId);
+		
+			System.out.println("DB에서 select 성공");
+			page = "/join/find_id_enterprise_result";// db값넣기 성공시 아이디확인 페이지로
+		} else {
+			System.out.println("DB에 select 실패");
+		}
+
+		return page;
+	}
+	
 	@RequestMapping("/find_pw") // 아이디 찾기
 	public String find_pw() {
 		return "/join/find_pw";
@@ -119,7 +117,36 @@ public class JoinController {
 
 		return page;
 	}
+	
+	@RequestMapping("/find_pw/find_pw_enterprise_result") // 개인 비밀번호 찾기 성공
+	public String find_pw_enterprise_result(Model model, HttpServletRequest request) {
+		String page = "/join/find_pw"; // 아이디 찾기 폼으로 ,
 
+		String enterprise_id = request.getParameter("id");
+		String manager_phone = request.getParameter("phone");
+
+		System.out.println(enterprise_id);		
+		System.out.println(manager_phone);
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("enterprise_id", enterprise_id);
+		map.put("manager_phone", manager_phone);
+
+		int updatePW = joinDAO.updateEnterprisePW(map);
+
+		System.out.println(updatePW);
+
+		if (updatePW==1) {
+
+			System.out.println("DB에서 update 성공");
+			page = "/join/find_pw_enterprise_result";// db값넣기 성공시 아이디확인 페이지로
+		} else {
+			System.out.println("DB에 update 실패");
+		}
+
+		return page;
+	}
+	
 	@RequestMapping("/join_individual") // 개인 회원가입 폼
 	public String join_individual() {
 		return "/join/join_individual";
