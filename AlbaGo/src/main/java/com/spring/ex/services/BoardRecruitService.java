3ce6859bdb2fb7f8c0cc89271324dd4f6dae479f
Song <joon1769@gmail.com>
2020-06-22 15:28:02 +0900

    song - service 어노테이션 추가

--- AlbaGo/src/main/java/com/spring/ex/services/BoardRecruitService.java
+++ AlbaGo/src/main/java/com/spring/ex/services/BoardRecruitService.java
@@ -3,31 +3,31 @@ package com.spring.ex.services;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.spring.ex.dao.BoardRecruitDAO;
 import com.spring.ex.dto.BoardRecruit;
 import com.spring.ex.dto.BoardRecruit1;
 import com.spring.ex.dto.Recruit_Content;
 
+@Service
 public class BoardRecruitService {
 
 	@Autowired
 	BoardRecruitDAO boardRecruitDAO;
-	
+
 	public int insertBoard_recruit(BoardRecruit boardrecruit) {
-		return boardRecruitDAO.insertBoard_recruit(boardrecruit); 	
+		return boardRecruitDAO.insertBoard_recruit(boardrecruit);
 	}
 
-	public ArrayList<BoardRecruit1> selectList(){
+	public ArrayList<BoardRecruit1> selectList() {
 		return boardRecruitDAO.selectList();
 	}
-	
-	
+
 	public Recruit_Content selectView(String id) {
 		return boardRecruitDAO.selectView(id);
 	}
-	
-	
+
 	public void deleteBoard(String id) {
 		boardRecruitDAO.deleteBoard(id);
 	}
--- AlbaGo/src/main/java/com/spring/ex/services/RecruitService.java
+++ AlbaGo/src/main/java/com/spring/ex/services/RecruitService.java
@@ -1,13 +1,14 @@
 package com.spring.ex.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.spring.ex.dao.RecruitDAO;
 
+@Service
 public class RecruitService {
 
-	@Autowired 
+	@Autowired
 	RecruitDAO recruitDAO;
-	
-	
+
 }
